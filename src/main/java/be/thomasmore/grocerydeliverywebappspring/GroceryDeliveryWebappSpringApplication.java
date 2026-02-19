be2f87de26c2dda6a11cbd79630a33436ebfd6dc
Pasang Giatso <pasang.giatso@student.thomasmore.be>
2026-02-19 12:31:05 +0100

    Create a .gitIgnore file

---
+++ src/main/java/be/thomasmore/grocerydeliverywebappspring/GroceryDeliveryWebappSpringApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.grocerydeliverywebappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryDeliveryWebappSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryDeliveryWebappSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/grocerydeliverywebappspring/GroceryDeliveryWebappSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.grocerydeliverywebappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryDeliveryWebappSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
